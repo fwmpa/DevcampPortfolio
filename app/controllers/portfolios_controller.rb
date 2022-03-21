dd216db14887e51894e09d4730d58c3c03056de2
Fernando Andrade <fernandoandrade@Fernandos-MacBook-Pro.local>
2022-03-21 20:38:54 -0300

    Implemented form configuration for neste attributes
    Signed-off-by: Fernando Andrade <fernandoandrade@Fernandos-MacBook-Pro.local>

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,12 +12,15 @@ class PortfoliosController < ApplicationController
   end
   
   def new
-    @portfolio_item = Portfolio.new
+    @portfolio_item = Portfolio.new         #build instanciantes 3 portfolio_item with technologies
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
     @portfolio_item =
-      Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+      Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
+        technologies_attributes: [:name]))
+        #allows to receive name attribute from techonologies 
 
     respond_to do |format|
       if @portfolio_item.save
