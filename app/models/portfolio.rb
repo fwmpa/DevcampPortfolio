2a44c07aad2360ff1ffa129c30d392e206cc1675
Fernando Andrade <fernandoandrade@Fernandos-MacBook-Pro.local>
2022-03-21 17:29:56 -0300

    Implemented concern for managing image data
    Signed-off-by: Fernando Andrade <fernandoandrade@Fernandos-MacBook-Pro.local>

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
 
     #Custom scopes
@@ -11,8 +12,8 @@ class Portfolio < ApplicationRecord
     after_initialize :set_defaults
 
     def set_defaults    # ||= if attribute == nil, set following value to the attribute
-        self.main_image ||= "https://via.placeholder.com/600x400"
-        self.thumb_image ||= "https://via.placeholder.com/350x150"
+        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     end
     
 end
